70e853da09e839d986204db4c816d6ab5f6fa300
idosun <ishemesh@sentry.io>
2020-03-25 18:43:40 -0700

    Initial Commit

---
+++ sentry-cocoa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sentry-cocoa
+//
+//  Created by Ido  Shemesh on 3/25/20.
+//  Copyright © 2020 Sentry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
